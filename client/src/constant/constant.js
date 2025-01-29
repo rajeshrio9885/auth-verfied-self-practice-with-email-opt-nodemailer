49d3dc442933ae85ec1778c6819580c39d434f6d
rajeshrio9885 <nirmalvicky62@gmail.com>
2025-01-29 23:51:15 +0530

    completed commit

--- client/src/constant/constant.js
+++ client/src/constant/constant.js
@@ -1,1 +1,2 @@
-export const url = "http://localhost:8000/api/auth"
+//export const url = "http://localhost:8000/api/auth"
+export const url = "https://auth-verfied-self-practice-with-email.onrender.com"
